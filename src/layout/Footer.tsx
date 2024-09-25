520c1145ea38569953e480e2adfbfe76d29c7410
T.S. Chief <fullstack.star0123@gmail.com>
2024-09-25 19:52:44 +0900

    Update Footer.tsx

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,6 @@ export default function Footer() {
     <footer className="mt-auto border-t border-white/10  px-4 py-8">
       <p className="text-center">
         &copy;
-        <a href="https://github.com/JohnMwendwa"> John Mwendwa </a>
         {new Date().getFullYear()}
       </p>
     </footer>
